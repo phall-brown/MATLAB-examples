cd07bef2448f34e2a62a43e9660191d147ec3779
Paul Hall <45795415+phall-brown@users.noreply.github.com>
2024-06-13 13:16:10 -0400

    Adding simple test of F_to_C function

---
+++ functions/test.m
@@ -0,0 +1,28 @@
+%Example script for bootcamp
+%Create temperature array in °F and convert it to
+%°C using the T_to_C function. Save the resulting values
+%in the array T_C.
+
+% clear workspace
+clear
+
+% temperature in °F
+T_F=[ 50.4
+52.2
+54.5
+58.1
+63.2
+65.5
+66.5
+53.2
+55.4
+58.2
+55.3
+50.3
+48.6
+45.3
+]';
+
+% convert temperature from °F to °C
+T_C=F_to_C(T_F);
+
